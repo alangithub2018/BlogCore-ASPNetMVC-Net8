bfdb54f285e0101989d460cd5bba38e91e4df431
Eric Alan Rodriguez Contreras <licalan.sistemas@gmail.com>
2025-01-18 10:59:47 -0600

    Agregar archivos de proyecto.

---
+++ BlogCore-ASPNetMVC-Net8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogCore_ASPNetMVC_Net8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
