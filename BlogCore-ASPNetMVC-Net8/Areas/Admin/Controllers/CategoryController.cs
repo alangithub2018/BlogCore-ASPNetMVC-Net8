7975b7ccc059e002611e10c5fce745e436afa4a5
Eric Alan Rodriguez Contreras <licalan.sistemas@gmail.com>
2025-01-20 15:54:04 -0600

    Finish edit and delete functionality using sweetalert and toastr plugins

--- BlogCore-ASPNetMVC-Net8/Areas/Admin/Controllers/CategoryController.cs
+++ BlogCore-ASPNetMVC-Net8/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BlogCore_ASPNetMVC_Net8.Data.Repository.IRepository;
+using BlogCore_ASPNetMVC_Net8.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogCore_ASPNetMVC_Net8.Areas.Admin.Controllers
@@ -19,11 +20,72 @@ namespace BlogCore_ASPNetMVC_Net8.Areas.Admin.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _workContainer.CategoryRepository.Add(category);
+                _workContainer.Save();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Category category = new Category();
+            category = _workContainer.CategoryRepository.Get(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _workContainer.CategoryRepository.Update(category);
+                _workContainer.Save();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
         #region API Calls
         [HttpGet]
         public IActionResult GetAll()
         {
             return Json(new { data = _workContainer.CategoryRepository.GetAll() });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _workContainer.CategoryRepository.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error deleting category" });
+            }
+
+            _workContainer.CategoryRepository.Remove(objFromDb);
+            _workContainer.Save();
+            return Json(new { success = true, message = "Category deleted successfully" });
         }
 
 
