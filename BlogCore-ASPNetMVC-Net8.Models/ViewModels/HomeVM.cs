f9954e0b019a6195b5189b69cfcf906ce63f6f11
Eric Alan Rodriguez Contreras <licalan.sistemas@gmail.com>
2025-01-21 21:15:18 -0600

    Added carousel sliders dinamically configured by admin interface

---
+++ BlogCore-ASPNetMVC-Net8.Models/ViewModels/HomeVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore_ASPNetMVC_Net8.Models.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Slider> Sliders { get; set; }
+        public IEnumerable<Article> Articles { get; set; }
+    }
+}
