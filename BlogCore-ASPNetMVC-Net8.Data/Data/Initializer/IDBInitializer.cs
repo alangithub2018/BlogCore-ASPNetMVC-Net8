76bbc353a8a5d8bcbbb5a15d2ac0002dd9871499
Eric Alan Rodriguez Contreras <licalan.sistemas@gmail.com>
2025-01-24 13:11:43 -0600

    Added DBInitializer class and minor code improvements

---
+++ BlogCore-ASPNetMVC-Net8.Data/Data/Initializer/IDBInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore_ASPNetMVC_Net8.Data.Data.Initializer
+{
+    public interface IDBInitializer
+    {
+        void Initialize();
+    }
+}
